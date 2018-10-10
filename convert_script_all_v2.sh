0ec728787d65fabb6852a218f198742dc46e5f3b
jrussell9000 <jrussell9000@gmail.com>
2018-10-10 17:50:59 -0500

    Started working on fieldmap correction

--- convert_script_all_v2.sh
+++ convert_script_all_v2.sh
@@ -20,14 +20,14 @@ else
 
 #####VARIABLES PASSED FROM COMMAND#####
 
-RAW_INPUT_DIR=$1 
+RAW_INPUT_DIR=$1
 echo "Input directory "$RAW_INPUT_DIR
 
 DATASET_DIR=$2
 echo "Output directory "$DATASET_DIR
 
 shift 2
-SUBJECTS_RAW=$* 
+SUBJECTS_RAW=$*
 
 #####GLOBALS#####
 
@@ -35,17 +35,17 @@ SUBJECTS=$(echo $SUBJECTS_RAW | tr -d "_")
 
 #####FUNCTION CALLS#####
 #convert() {
-#  
+#
 #  for scanpath in ${RAW_INPUT_DIR}/_${i}/dicoms/*/; do
 #      #Verify that only directories are considered, per https://unix.stackexchange.com/questions/86722/how-do-i-loop-through-only-directories-in-bash
 #      if [ -d ${scanpath} ]; then
 #	continue
 #      fi
-#		  
-#      SEQ_GESCANTYPE=$(basename ${scanpath}) 
-#      
-#      GE_SCANTYPE=`basename ${scanpath} | awk 'BEGIN {FS="_"} {print $NF}'`		
-#	
+#
+#      SEQ_GESCANTYPE=$(basename ${scanpath})
+#
+#      GE_SCANTYPE=`basename ${scanpath} | awk 'BEGIN {FS="_"} {print $NF}'`
+#
 #      IMAGE_DESC=$(grep -A 2 "_${i}/dicoms/${SEQ_SCANTYPE}" ${RAW_INPUT_DIR}/_${i}/dicoms/info.txt | tail -n 1 )
 #
 #      case "$IMAGE_DESC" in
@@ -56,53 +56,51 @@ SUBJECTS=$(echo $SUBJECTS_RAW | tr -d "_")
 #	  MAPTYPE=EPI
 #	;;
 #      esac
-#      
+#
 #      tmppath_check
 #
-#	cp -fr ${scanpath} /tmp	
-#		  
-#	decompress 
+#	cp -fr ${scanpath} /tmp
+#
+#	decompress
 #	rm ${SUBJ_DIR}/ANAT/*T1*
 #
-#	dcm2niix -b y -f %i_${SCANTYPE}_T1 -o ${SUBJ_DIR}/ANAT/ /tmp/$(basename ${scanpath})	
+#	dcm2niix -b y -f %i_${SCANTYPE}_T1 -o ${SUBJ_DIR}/ANAT/ /tmp/$(basename ${scanpath})
 #
 #    done
 #}
 
 #If $scanpath already exists in /tmp, delete it
 tmppath_check() {
-  if [ -d /tmp/$(basename ${scanpath}) ]; then
-    rm -rf /tmp/$(basename ${scanpath})
+  if [ -d /tmp/"${scanpath##*/}" ]; then
+    rm -rf /tmp/"${scanpath##*/}"
   fi
 }
 
 #Decompress bz2 DICOM files
 decompress() {
-  for z in /tmp/$(basename ${scanpath})/*.bz2; do
+  for z in /tmp/"${scanpath##*/}"/*.bz2; do
     bzip2 -d $z
   done
 }
-
-echo ~~~Convert File~~~;
 
 for i in ${SUBJECTS}; do
 
 #Making directories to hold per subject SCANS
   SUBJ_PATH=${DATASET_DIR}/${i}
-  rm -rf ${SUBJ_PATH} 
-  mkdir -p -v ${SUBJ_PATH}
+  rm -rf ${SUBJ_PATH}
+  mkdir -p ${SUBJ_PATH}
 
   rm -rf ${SUBJ_PATH}/ANAT
-  mkdir -p -v ${SUBJ_PATH}/ANAT 
- 
+  mkdir -p ${SUBJ_PATH}/ANAT
+
   rm -rf ${SUBJ_PATH}/DTI
-  mkdir -p -v ${SUBJ_PATH}/DTI 
- 
+  mkdir -p ${SUBJ_PATH}/DTI
+
   rm -rf ${SUBJ_PATH}/FMAP
-  mkdir -p -v ${SUBJ_PATH}/FMAP  
- 
+  mkdir -p ${SUBJ_PATH}/FMAP
+
   rm -rf ${SUBJ_PATH}/INFO
-  mkdir -p -v ${SUBJ_PATH}/INFO 
+  mkdir -p ${SUBJ_PATH}/INFO
 
 done
 
@@ -110,107 +108,114 @@ cd ${RAW_INPUT_DIR} || exit
 
 for i in ${SUBJECTS}; do
 
-  cd ${RAW_INPUT_DIR}/_${i} || continue 
-  
-  echo ~~~Subject in process: ${i}~~~
+  cd ${RAW_INPUT_DIR}/_${i} || continue
+
+  printf "\n%s\n" "/////////////////////////////////////////"
+  printf "%s" "////////SUBJECT BEING PROCESSED: ${i}/////////"
+  printf "\n%s\n" "/////////////////////////////////////////"
 
   SUBJ_DIR=${DATASET_DIR}/${i}
 
   cp ${RAW_INPUT_DIR}/_${i}/dicoms/info.txt ${SUBJ_DIR}/INFO/${i}_info.txt;
 
-  echo "~~~~~Converting anatomical scans~~~~~~"
+  printf "\n%s\n" "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+  printf "%s" "~~~~~CONVERTING ANATOMICAL SCANS~~~~~~"
+  printf "\n%s\n" "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    printf "\n%s\n" "-------Starting with T1 (BRAVO)-------"
 
-    echo "-------Starting T1-------"
-
-    for scanpath in ${RAW_INPUT_DIR}/_${i}/dicoms/*bravo/; do
+    for scanpath in ${RAW_INPUT_DIR}/_${i}/dicoms/*bravo; do
       #Verify that only directories are considered, per https://unix.stackexchange.com/questions/86722/how-do-i-loop-through-only-directories-in-bash
       if [ -d ${scanpath} ]; then
-		  
-	SCANTYPE=`basename ${scanpath} | awk 'BEGIN {FS="_"} {print $NF}'`	
-		  
-	tmppath_check
 
-	cp -fr ${scanpath} /tmp	
-		  
-	decompress 
+      	SCANTYPE=`basename ${scanpath} | awk 'BEGIN {FS="_"} {print $NF}'`
 
-	dcm2niix -b y -z y -f ${i}_${SCANTYPE}_T1 -o ${SUBJ_DIR}/ANAT/ /tmp/$(basename ${scanpath})	
-      
+      	tmppath_check
+
+      	cp -fr ${scanpath} /tmp
+
+      	decompress
+
+      	dcm2niix -b y -z y -f ${i}_${SCANTYPE}_T1 -o ${SUBJ_DIR}/ANAT/ /tmp/"${scanpath##*/}"
+
       fi
 
     done
 
-    echo "-------Starting T2-------"
+    printf "\n%s\n" "-------Starting with T2 (3DIR)-------"
 
-    for scanpath in ${RAW_INPUT_DIR}/_${i}/dicoms/*3dir/; do
+    for scanpath in ${RAW_INPUT_DIR}/_${i}/dicoms/*3dir; do
       #Verify that only directories are considered, per https://unix.stackexchange.com/questions/86722/how-do-i-loop-through-only-directories-in-bash
       if [ -d ${scanpath} ]; then
-	      
-	SCANTYPE=`basename ${scanpath} | awk 'BEGIN {FS="_"} {print $NF}'`	
-      
-	tmppath_check
 
-	cp -fr ${scanpath} /tmp	
-      
-	decompress 
+	      SCANTYPE=`basename ${scanpath} | awk 'BEGIN {FS="_"} {print $NF}'`
 
-	dcm2niix -b y -z y -f ${i}_${SCANTYPE}_T2 -o ${SUBJ_DIR}/ANAT/ /tmp/$(basename ${scanpath})	
-      
+      	tmppath_check
+
+      	cp -fr ${scanpath} /tmp
+
+      	decompress
+
+      	dcm2niix -b y -z y -f ${i}_${SCANTYPE}_T2 -o ${SUBJ_DIR}/ANAT/ /tmp/"${scanpath##*/}"
+
       fi
-    
+
     done
-    
-    echo "~~~~~Converting DTI scans~~~~~" 
-    
-    for scanpath in ${RAW_INPUT_DIR}/_${i}/dicoms/{*dti,*FA,*ADC,*CMB}/; do
+
+    printf "\n%s\n" "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    printf "%s" "~~~~~~~~~CONVERTING DTI SCANS~~~~~~~~~~"
+    printf "\n%s\n" "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
+    for scanpath in ${RAW_INPUT_DIR}/_${i}/dicoms/{*dti,*FA,*ADC,*CMB}; do
       #Verify that only directories are considered, per https://unix.stackexchange.com/questions/86722/how-do-i-loop-through-only-directories-in-bash
       if [ -d ${scanpath} ]; then
-	      
-	SCANTYPE=`basename ${scanpath} | awk 'BEGIN {FS="_"} {print $NF}'`	
-      
-	tmppath_check
 
-	cp -fr ${scanpath} /tmp	
-      
-	decompress 
+      	SCANTYPE=`basename ${scanpath} | awk 'BEGIN {FS="_"} {print $NF}'`
 
-	dcm2niix -b y -z y -f ${i}_${SCANTYPE}_DTI -o ${SUBJ_DIR}/DTI/ /tmp/$(basename ${scanpath})
+      	tmppath_check
+
+      	cp -fr ${scanpath} /tmp
+
+      	decompress
+
+      	dcm2niix -b y -z y -f ${i}_${SCANTYPE}_DTI -o ${SUBJ_DIR}/DTI/ /tmp/"${scanpath##*/}"
 
       fi
-    
+
     done
 
-    echo "~~~~~Converting Field Maps~~~~~"
+    printf "\n%s\n" "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    printf "%s" "~~~~~~~~~CONVERTING FIELDMAPS~~~~~~~~~~"
+    printf "\n%s\n" "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 
-    for scanpath in ${RAW_INPUT_DIR}/_${i}/dicoms/*fmap/; do
+    for scanpath in ${RAW_INPUT_DIR}/_${i}/dicoms/*fmap; do
                 #Verify that only directories are considered, per https://unix.stackexchange.com/questions/86722/how-do-i-loop-through-only-directories-in-bash
       if [ -d ${scanpath} ]; then
-	      
-	echo ${scanpath}
-	#Problem: Multiple fieldmap directories exist for each subject
-	#Solution: Parse each subjects info.txt file to determine which is which
-	
-	IMAGE_DESC=$(grep -A 2 "_${i}/dicoms/$(basename ${scanpath})" ${RAW_INPUT_DIR}/_${i}/dicoms/info.txt | tail -n 1 )
-	echo $IMAGE_DESC
-	case "$IMAGE_DESC" in
-		*DTI*)
-			MAPTYPE=DTI
-			;;
-		*EPI*)
-			MAPTYPE=EPI
-			;;
-	esac
-	
-	tmppath_check
 
-	cp -fr ${scanpath} /tmp	
-      
-	decompress 
+      	echo ${scanpath}
+      	#Problem: Multiple fieldmap directories exist for each subject
+      	#Solution: Parse each subjects info.txt file to determine which is which
 
-	dcm2niix -b y -z y -m y -f ${i}_${SCANTYPE}_FMAP_${MAPTYPE} -o ${SUBJ_DIR}/FMAP/ /tmp/$(basename ${scanpath}) 
-	
+      	IMAGE_DESC=$(grep -A 2 "_${i}/dicoms/${scanpath##*/}" ${RAW_INPUT_DIR}/_${i}/dicoms/info.txt | tail -n 1 )
+
+      	case "$IMAGE_DESC" in
+      		*DTI*)
+      			MAPTYPE=DTI
+      			;;
+      		*EPI*)
+      			MAPTYPE=EPI
+      			;;
+      	esac
+
+      	tmppath_check
+
+      	cp -fr ${scanpath} /tmp
+
+      	decompress
+
+      	dcm2niix -b y -z y -m y -f ${i}_${SCANTYPE}_FMAP_${MAPTYPE} -o ${SUBJ_DIR}/FMAP/ /tmp/"${scanpath##*/}"
+
       fi
-    
+
     done
 
 done
