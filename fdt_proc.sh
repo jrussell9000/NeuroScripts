a78f6a31f07479b7927068a578cee3d87b3a621f
Justin <jrussell9000@gmail.com>
2018-10-31 21:47:09 -0500

    Updates

--- fdt_proc.sh
+++ fdt_proc.sh
@@ -32,38 +32,50 @@ parse_subjs() {
   fi
 }
 
-makeprocdir () {
-mkdir "${SUBJ}"/fslproc
-SUBJ_PATH="${PROC_DIR}"/"${SUBJ}"
-FSL_PROC="${SUBJ_PATH}"/fslproc
-cp "${SUBJ}"/ANAT/* "${SUBJ}"/fslproc
-cp "${SUBJ}"/FMAP/* "${SUBJ}"/fslproc
+make_proc_dir() {
+	SUBJ_PATH="${PROC_DIR}"/"${SUBJ}"
+  if [ -d "${SUBJ_PATH}"/fsl_proc ]; then
+    rm -rf "${SUBJ_PATH}"/fsl_proc
+	  mkdir -p "${SUBJ_PATH}"/fsl_proc
+  else
+    mkdir -p "${SUBJ_PATH}"/fsl_proc
+  fi
+  FSL_PROC="${SUBJ_PATH}"/fsl_proc
 }
 
+proc_prep() {
+  cp "${SUBJ_PATH}"/DTI/* "${FSL_PROC}"
+  cp "${SUBJ_PATH}"/FMAP/* "${FSL_PROC}"
+}
 
-bet() {
-  bet "${SUBJ}"_DTI.nii "${SUBJ}"_DTI_bet.nii -m
+brain_extract() {
+  bet "${SUBJ}"_DTI_fm "${SUBJ}"_DTI_fm_bet_temp
+  fslmaths "${SUBJ}"_DTI_fm -mas "${SUBJ}"_DTI_fm_bet_temp "${SUBJ}"_DTI_fm_bet
 }
 
 denoise() {
-  mrconvert -json_import "${SUBJ}"_DTI.json -fslgrad "${SUBJ}"_DTI.bvec "${SUBJ}"_DTI.bval "${SUBJ}"_DTI_bet.nii "${SUBJ}"_DTI_bet.mif
-  dwidenoise "${SUBJ}"_DTI_bet.mif "${SUBJ}"_DTI_bet_den.mif -noise "${SUBJ}"_DTI_noise.mif
-  mrcalc "${SUBJ}"_DTI_bet.mif "${SUBJ}"_DTI_bet_den.mif -subtract "${SUBJ}"_DTI_residual.mif
-  mrdegibbs "${SUBJ}"_DTI_bet_den.mif "${SUBJ}"_DTI_bet_den_deg.mif
-  mrconvert "${SUBJ}"_DTI_bet_den_deg.mif "${SUBJ}"_DTI_bet_den_deg.nii
+  mrconvert -json_import "${SUBJ}"_DTI.json -fslgrad "${SUBJ}"_DTI.bvec "${SUBJ}"_DTI.bval "${SUBJ}"_DTI_fm_bet.nii.gz "${SUBJ}"_DTI_fm_bet.mif
+  dwidenoise "${SUBJ}"_DTI_fm_bet.mif "${SUBJ}"_DTI_fm_bet_den.mif -noise "${SUBJ}"_DTI_noise.mif
+  mrcalc "${SUBJ}"_DTI_fm_bet.mif "${SUBJ}"_DTI_fm_bet_den.mif -subtract "${SUBJ}"_DTI_residual.mif
+  mrdegibbs "${SUBJ}"_DTI_fm_bet_den.mif "${SUBJ}"_DTI_fm_bet_den_deg.mif
+  mrconvert "${SUBJ}"_DTI_fm_bet_den_deg.mif "${SUBJ}"_DTI_fm_bet_den_deg.nii.gz
 }
 
 # 0 1 for A>>P phase encoding
 create_acqp_index() {
-  printf "0 1 0 0.05" >> acqparams.txt
+  printf "0 1 0 0.05" >> "${FSL_PROC}"/acqparams.txt
   indx=""
   for ((i=1; i<=56; i+=1)); do indx="$indx 1"; done
   echo "$indx" > index.txt
 }
 
+make_mask() {
+  fslmaths "${SUBJ}"_DTI_fm_bet -bin "${SUBJ}"_DTI_mask
+}
+
 eddy() {
-  eddy_openmp --imain="${SUBJ}"_DTI_bet_den_deg.nii --mask="${SUBJ}"_DTI_bet_mask.nii.gz --acqp=acqparams.txt --field="${SUBJ}"_DTI_FMAP.nii \
-  --index=index.txt --bvecs="${SUBJ}"_DTI.bvec --bval="${SUBJ}"_DTI.bval --repol --out="${SUBJ}"_DTI_bet_den_deg_eddy
+  eddy_cuda --imain="${SUBJ}"_DTI_fm_bet_den_deg --mask="${SUBJ}"_DTI_bet_mask --acqp=acqparams.txt --field="${SUBJ}"_DTI_FMAP \
+  --index=index.txt --bvecs="${SUBJ}"_DTI.bvec --bvals="${SUBJ}"_DTI.bval --repol --out="${SUBJ}"_DTI_bet_den_deg_eddy
 }
 
 ######MAIN######
@@ -71,9 +83,10 @@ parse_subjs
 
 for SUBJ in "${subj_array[@]}"; do
 	subj_start
-  makeprocdir
+  make_proc_dir
+  proc_prep
   pushd "${FSL_PROC}" || continue
-  bet
+  brain_extract
   denoise
   create_acqp_index
   eddy
