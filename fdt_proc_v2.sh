fa331c03c5561dc5d2c551a295e202f9e5109c25
Justin <jrussell9000@gmail.com>
2018-11-13 22:20:27 -0600

    Added topup functionality

--- fdt_proc_v2.sh
+++ fdt_proc_v2.sh
@@ -146,9 +146,11 @@ main() {
 
   get_options "$@"
 
-  DWIPROC="DWI_processing"
+  DWIPROC="dwi_processing"
   INPUT_DIR=$STUDY_DIR/$SUBJECT/dicoms
   OUTPUT_DIR=$STUDY_DIR/$SUBJECT/$DWIPROC
+  raw_dir=$OUTPUT_DIR/raw
+  topup_dir=$OUTPUT_DIR/topup
   pepositive="pepolar0"
   penegative="pepolar1"
   hyperband="HB2"
@@ -159,8 +161,8 @@ main() {
   fi
 
   mkdir -p "$OUTPUT_DIR"
-  mkdir "${OUTPUT_DIR}"/original
   mkdir "${OUTPUT_DIR}"/raw
+  mkdir "${OUTPUT_DIR}"/topup
 
   if [ ! -d "$OUTPUT_DIR" ]; then
     echo "ERROR: Failed to create output directory.  Do you have write permissions for the directory ${STUDY_DIR}/${SUBJECT}?"
@@ -171,59 +173,37 @@ main() {
   # "raw" subdirectory of the output directory.  Rename the scans according to their phase encoding direction (pepolar0 or pepolar1).
   # If the scans were created using hyperband, label the conversion files as such.
 
-  for file in "$INPUT_DIR"/*.NODDI*.tgz; do
-    if [[ "$HYPERBAND_OPT" == "Y" ]]; then
-      if [[ "$file" == *"$pepositive"* && "$file" == *"$hyperband"* ]]; then
-        tmp_dir
-        cp "$file" "$TMP"
-        tar xf "$TMP"/"$file" -C "$TMP"
-        dcm2niix "$TMP"
-        "${FSL_DIR}"/bin/imcp "$TMP"/*.nii "$OUTPUT_DIR"/raw/"$hyperband"_"$pepositive".nii
-        cp "$TMP"/*.bval "$OUTPUT_DIR"/raw/"$hyperband"_"$pepositive".bval
-        cp "$TMP"/*.bvec "$OUTPUT_DIR"/raw/"$hyperband"_"$pepositive".bvec
-        cp "$TMP"/*.json "$OUTPUT_DIR"/raw/"$hyperband"_"$pepositive".json
-        rm -rf "$TMP"
-      fi
-
-      if [[ "$file" == *"$penegative"* && "$file" == *"$hyperband"* ]]; then
-        tmp_dir
-        cp "$file" "$TMP"
-        tar xf "$TMP"/"$file" -C "$TMP"
-        dcm2niix "$TMP"
-        "${FSL_DIR}"/bin/imcp "$TMP"/*.nii "$OUTPUT_DIR"/raw/"$hyperband"_"$penegative".nii
-        cp "$TMP"/*.bval "$OUTPUT_DIR"/raw/"$hyperband"_"$penegative".bval
-        cp "$TMP"/*.bvec "$OUTPUT_DIR"/raw/"$hyperband"_"$penegative".bvec
-        cp "$TMP"/*.json "$OUTPUT_DIR"/raw/"$hyperband"_"$penegative".json
-        rm -rf "$TMP"
-      fi
-    fi
-    
-    if [[ "$HYPERBAND_OPT" == "N" ]]; then
-      if [[ "$file" == *"$pepositive"* ]]; then
-          tmp_dir
-          cp "$file" "$TMP"
-          tar xf "$TMP"/*.tgz -C "$TMP"
-          dcm2niix "$TMP"
-          "${FSL_DIR}"/bin/imcp "$TMP"/*.nii "$OUTPUT_DIR"/raw/"$pepositive".nii
-          cp "$TMP"/*.bval "$OUTPUT_DIR"/raw/"$pepositive".bval
-          cp "$TMP"/*.bvec "$OUTPUT_DIR"/raw/"$pepositive".bvec
-          cp "$TMP"/*.json "$OUTPUT_DIR"/raw/"$pepositive".json
-          rm -rf "$TMP"
-        fi
-
-      if [[ "$file" == *"$penegative"* ]]; then
-        tmp_dir
-        cp "$file" "$TMP"
-        tar xf "$TMP"/*.tgz -C "$TMP"
-        dcm2niix "$TMP"
-        "${FSL_DIR}"/bin/imcp "$TMP"/*.nii "$OUTPUT_DIR"/raw/"$penegative".nii
-        cp "$TMP"/*.bval "$OUTPUT_DIR"/raw/"$penegative".bval
-        cp "$TMP"/*.bvec "$OUTPUT_DIR"/raw/"$penegative".bvec
-        cp "$TMP"/*.json "$OUTPUT_DIR"/raw/"$penegative".json
-        rm -rf "$TMP"
-      fi
-    fi
+  if [[ "$HYPERBAND_OPT" == "Y" ]]; then
+    pos_enc="NODDI_HB2_pepolar0"
+    neg_enc="NODDI_HB2_pepolar1"
+  else
+    pos_enc="NODDI_pepolar0"
+    neg_enc="NODDI_pepolar1"
+  fi
+  
+  for file in "$INPUT_DIR"/*"${pos_enc}"*.tgz; do
+    tmp_dir
+    cp "$file" "$TMP"
+    tar xf "$TMP"/"$(basename $file)" -C "$TMP" 
+    dcm2niix -z y "$TMP"
+    imcp "$TMP"/*.nii.gz "$OUTPUT_DIR"/raw/"$pos_enc".nii.gz
+    #cp "$TMP"/*.bval "$OUTPUT_DIR"/raw/"$pos_enc".bval - Must use the bval and bvec files from the scanner, values in dicoms are incorrect
+    #cp "$TMP"/*.bvec "$OUTPUT_DIR"/raw/"$pos_enc".bvec
+    cp "$TMP"/*.json "$OUTPUT_DIR"/raw/"$pos_enc".json
   done
+
+  for file in "$INPUT_DIR"/*"${neg_enc}"*.tgz; do
+    tmp_dir
+    cp "$file" "$TMP"
+    tar xf "$TMP"/"$(basename $file)" -C "$TMP" 
+    dcm2niix -z y "$TMP"
+    imcp "$TMP"/*.nii.gz "$OUTPUT_DIR"/raw/"$neg_enc".nii.gz
+    #cp "$TMP"/*.bval "$OUTPUT_DIR"/raw/"$neg_enc".bval
+    #cp "$TMP"/*.bvec "$OUTPUT_DIR"/raw/"$neg_enc".bvec
+    cp "$TMP"/*.json "$OUTPUT_DIR"/raw/"$neg_enc".json
+  done
+
+  #COMPUTE TOTAL READOUT TIME
 
   # Computing the total readout time in seconds, and up to six decimal places. Grab any
   # scan in the raw directory and get the second dimension (slice count if using PA/AP phase encoding)
@@ -235,35 +215,76 @@ main() {
   totalrotime=$(awk "BEGIN {print ${ECHOSPACING}*${dimsmin1}; exit}" )
   totalrotime=$(awk "BEGIN {print ${totalrotime} / 1000; exit}" )
 
-  # Extract b0's and make acqparms.txt for topup
-  for file in "${OUTPUT_DIR}"/raw/"${pepositive}".nii.gz; do
-    fslroi "${file}" "${OUTPUT_DIR}"/raw/Pos_b0 0 4
-    b0vols=$(${FSL_DIR}/bin/fslval "${OUTPUT_DIR}"/raw/Pos_b0 dim4)
+
+  #TOPUP
+
+  #-Extract b0's and make acqparms.txt for topup
+  for file in "${OUTPUT_DIR}"/raw/"${pos_enc}".nii.gz; do
+    fslroi "${file}" "${OUTPUT_DIR}"/raw/pos_b0 0 4
+    b0vols=$(${FSL_DIR}/bin/fslval "${OUTPUT_DIR}"/raw/pos_b0 dim4)
     for (( i=1; i<=b0vols; i++ )); do
-      echo 0 1 0 "$totalrotime" >> "${OUTPUT_DIR}"/raw/acqparms.txt
+      echo 0 1 0 "$totalrotime" >> "${OUTPUT_DIR}"/raw/acqparams.txt
     done
   done
 
-  for file in "${OUTPUT_DIR}"/raw/"${penegative}".nii.gz; do
-    fslroi "${file}" "${OUTPUT_DIR}"/raw/Neg_b0 0 4
-    b0vols=$(${FSL_DIR}/bin/fslval "${OUTPUT_DIR}"/raw/Neg_b0 dim4)
+  for file in "${OUTPUT_DIR}"/raw/"${neg_enc}".nii.gz; do
+    fslroi "${file}" "${OUTPUT_DIR}"/raw/neg_b0 0 4
+    b0vols=$(${FSL_DIR}/bin/fslval "${OUTPUT_DIR}"/raw/neg_b0 dim4)
     for (( i=1; i<=b0vols; i++ )); do
-      echo 0 -1 0 "$totalrotime" >> "${OUTPUT_DIR}"/raw/acqparms.txt
+      echo 0 -1 0 "$totalrotime" >> "${OUTPUT_DIR}"/raw/acqparams.txt
     done
   done
 
-  # Merge files
-  fslmerge -t "${OUTPUT_DIR}"/raw/Pos_Neg_b0 "${OUTPUT_DIR}"/raw/Pos_b0 "${OUTPUT_DIR}"/raw/Neg_b0
-  fslmerge -t "${OUTPUT_DIR}"/raw/Pos_Neg "${OUTPUT_DIR}"/raw/pepolar0 "${OUTPUT_DIR}"/raw/pepolar1
+  #-Merge separate b0 files 
+  fslmerge -t "${OUTPUT_DIR}"/raw/pos_neg_b0 "${OUTPUT_DIR}"/raw/pos_b0 "${OUTPUT_DIR}"/raw/neg_b0
 
-  ###!!!!!!!!!!DELETE DCM2NIIX CREATED BVAL AND BVEC FILES - REMOVE THIS SECTION ONCE STEVE PUTS CORRECT VALUES IN THE DICOM!!!!###
-  rm "${OUTPUT_DIR}"/raw/*.bvec
-  rm "${OUTPUT_DIR}"/raw/*.bval
-  cp "${STUDY_DIR}"/"${SUBJECT}"/NODDI_pepolar0.bval "${OUTPUT_DIR}"/raw/"$pepositive".bval
-  cp "${STUDY_DIR}"/"${SUBJECT}"/NODDI_pepolar0.bvec "${OUTPUT_DIR}"/raw/"$pepositive".bvec
-  cp "${STUDY_DIR}"/"${SUBJECT}"/NODDI_pepolar1.bval "${OUTPUT_DIR}"/raw/"$penegative".bval
-  cp "${STUDY_DIR}"/"${SUBJECT}"/NODDI_pepolar1.bvec "${OUTPUT_DIR}"/raw/"$penegative".bvec
 
+  #-copying necessary files to topup directory for further processing
+  imcp "${raw_dir}"/pos_b0 "${topup_dir}"
+  imcp "${raw_dir}"/neg_b0 "${topup_dir}"
+  imcp "${raw_dir}"/pos_neg_b0 "${topup_dir}"
+  cp "${raw_dir}"/acqparams.txt "${topup_dir}"
+
+  #-Run topup using the combined b0 file
+  topup -v --imain="${topup_dir}"/pos_neg_b0 --datain="${topup_dir}"/acqparams.txt --config=b02b0.cnf --out="${topup_dir}"/topup_pos_neg_b0
+
+  #-Per HCP script (run_topup.sh), run applytopup to first b0 from positive and negative phase encodings to generate a hifib0 which will
+  #-be used to create the brain mask.  
+  fslroi "${topup_dir}"/pos_b0 "${topup_dir}"/pos_b0_1st 0 1
+  fslroi "${topup_dir}"/neg_b0 "${topup_dir}"/neg_b0_1st 0 1
+
+  dimt=$(fslval "${topup_dir}"/Pos_b0 dim4)
+  dimt=$(("${dimt}" + 1))
+
+  #-applytopup must use the jacobian modulation method (--method=jac) since the diffusion gradients do not match one-to-one across the phase encodings
+  applytopup --imain="${topup_dir}"/pos_b0_1st,"${topup_dir}"/neg_b0_1st --method=jac --topup="${topup_dir}"/topup_pos_neg_b0 --datain="${topup_dir}"/acqparams.txt --inindex=1,"${dimt}" --out="${topup_dir}"/hifib0
+ 
+  bet "${topup_dir}"/hifib0 "${topup_dir}"/nodif_brain -m -f 0.2
+
+  #EDDY
+
+  #-Converting the crappy diffusion vector and diffusion weight files we get from the scanner
+  
+  #--Starting with the vectors first....
+  #---Getting the number of columns in the vector file as "numcols"
+  numcols=$(($(head -n 1 "$BVEC_PATH" | grep -o " " | wc -l) + 1))
+
+  #---Starting with the third column (the first containing coordinates), loop over the remaining columns, and for each one cut it, remove the last entry
+  #---(which is a volume that doesn't exist), then echo it into a new text file.  The last step will transpose it from a column to a space-delimited row) 
+  
+  for file in "${BVEC_PATH}"
+
+  #???? How the fuck do I properly rename the files ????  Need to parse the info file for the scan, get the time value for each scan, reformat them,
+  #and then match them to each file.  FML.
+
+  for ((i=3;i<="$NUMC";i++)); do 
+      TEMP=$(cut -d" " -f"$i" "$BVEC_PATH")
+      TEMP=$(awk '{$NF=""}1' <(echo $TEMP))
+      echo $TEMP >> "$OUTPUT"
+      #TEMP=$(paste -s -d" " <(echo "$TEMP"))
+      #TEMP=$(awk '{$NF=""}1'); 
+      #echo "$TEMP" >> "$OUTPUT"."$TYPE"
+  done
 }
 
 main "$@"
