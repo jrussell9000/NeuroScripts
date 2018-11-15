a67e682235f4702a1fb06d9043158e47af6e2902
Justin <jrussell9000@gmail.com>
2018-11-14 22:37:26 -0600

    One more tweak

--- fdt_proc_v2.sh
+++ fdt_proc_v2.sh
@@ -318,24 +318,25 @@ main() {
     fi
   done
 
-  parse_list=$(cat "${INPUT_DIR}"/info.*.txt | grep -A3 "NODDI")
+  info_file=$(find ${INPUT_DIR} -maxdepth 1 -name "*.txt" -printf '%P\n')
+  parse_list=$(cat ${INPUT_DIR}/${info_file} | grep -A3 "NODDI")
   echo "$parse_list" | awk 'BEGIN {RS="--"} {print ($2" "$8)}' > seq_times.txt
 
   while read -r line; do
-      seq=$(echo "${line}" | cut -f1 -d" ")
-      time=$(echo "${line}" | cut -f2 -d" " | cut -c-4)
-      echo "$seq"
-      echo "$time"
-      for bvalfile in $(find "$GRADINFO_PATH" -name "*.bval" -printf '%P\n'); do
-          if [[ "${bvalfile}" == *"${time}"* ]]; then
-              mv "${bvalfile}" "${seq}".bval
-          fi
-      done
-      for bvecfile in $(find "$GRADINFO_PATH" -name "*.bvec" -printf '%P\n'); do
-          if [[ "${bvecfile}" == *"${time}"* ]]; then
-              mv "${bvecfile}" "${seq}".bvec
-          fi
-      done
+    seq=$(echo "${line}" | cut -f1 -d" ")
+    time=$(echo "${line}" | cut -f2 -d" " | cut -c-4)
+    echo "$seq"
+    echo "$time"
+    for bvalfile in $(find "$GRADINFO_PATH" -name "*.bval" -printf '%P\n'); do
+      if [[ "${bvalfile}" == *"${time}"* ]]; then
+          mv "${bvalfile}" "${seq}".bval
+      fi
+    done
+    for bvecfile in $(find "$GRADINFO_PATH" -name "*.bvec" -printf '%P\n'); do
+      if [[ "${bvecfile}" == *"${time}"* ]]; then
+          mv "${bvecfile}" "${seq}".bvec
+      fi
+    done
   done < seq_times.txt
 
   rm seq_times.txt
