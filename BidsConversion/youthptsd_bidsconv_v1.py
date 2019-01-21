0a89ad7657dfb46f45b115ef32ab24f18f5b186b
Justin <jrussell9000@gmail.com>
2019-01-20 21:56:20 -0600

    Ready for final testing...

--- BidsConversion/youthptsd_bidsconv_v1.py
+++ BidsConversion/youthptsd_bidsconv_v1.py
@@ -1,45 +1,45 @@
 #!/usr/bin/env python3
 import os
 import sys
-import tarfile
 import shutil
 import argparse
 import subprocess
 import tempfile
 import bz2
 import json
-from pathlib import Path, PurePath
+from pathlib import Path
 from distutils.dir_util import copy_tree
-import nipype
-from nipype.interfaces import afni
-from ast import literal_eval
+import yaml
+import numpy
+from numpy import ndarray
+import pydicom as dicom
+import _pickle as cPickle
+
+# - Utility function to underline strings
+def stru(string):
+    start = '\033[4m'
+    end = '\033[0m'
+    ustr = start + string + end
+    return(ustr)
+
+
+def printu(self, string):
+    start = '\033[4m'
+    end = '\033[0m'
+    ustr = start + string + end
+    print(ustr)
 
 
 class BidsConv():
 
-    def printu(self, string):
-        start = '\033[4m'
-        end = '\033[0m'
-        ustr = start + string + end
-        print(ustr)
-
     scanstoskip = ('cardiac', 'ssfse', 'ADC', 'FA', 'CMB',
-                   'assetcal', '3dir', 'dti', 'fse', 'bravo')
-    anatomicalscans = ('bravo', 'fse')
-    functionalscans = ('epi')
-    dwiscans = ('dwi')
-    fieldmapscans = ('fmap')
+                   'assetcal', '3dir')
 
     data_description = {
         "Name": "Youth PTSD",
         "BIDSVersion": "1.1.1",
         "License": "None",
-    }
-
-    bids_taskrun = 0
-
-    def __init__(self):
-        self.verbose = False
+    }  
 
     def initialize(self):
         ap = argparse.ArgumentParser()
@@ -85,11 +85,11 @@ class BidsConv():
 
     def scan2helpful(self, typestring):
         scan2helpful_dict = {
-            "bravo": "ANATOMICAL - T1w",
-            "fse": "ANATOMICAL - T2w",
-            "epi": "FUNCTIONAL",
-            "dti": "DIFFUSION WEIGHTED",
-            "fmap": "FIELD MAP"
+            "bravo": "AN ANATOMICAL - T1w",
+            "fse": "AN ANATOMICAL - T2w",
+            "epi": "A FUNCTIONAL",
+            "dti": "A DIFFUSION WEIGHTED",
+            "fmap": "A FIELD MAP"
         }
         returnkey = "UNKNOWN"
         for key in scan2helpful_dict.keys():
@@ -99,59 +99,65 @@ class BidsConv():
 
     def get_subj_dcms(self):
         self.subjID = self.subjID_dirname.replace("_", "")
-        if not self.subjID.__contains__('rescan'):
-            self.wave_no = 1
-        else:
+        if self.subjID.__contains__('rescan'):
             self.wave_no = 2
             self.subjID = self.subjID.replace("rescan", "")
+        else:
+            self.wave_no = 1
         subjID_path = os.path.join(self.studypath, self.subjID_dirname)
         self.dicomspath = Path(subjID_path, "dicoms")
-        self.tmpdir = tempfile.mkdtemp(suffix=self.subjID)
+        self.subjID_tmpdir = tempfile.mkdtemp(suffix=self.subjID)
 
+    # Copying the bzip files to /tmp/<scan_type> and decompressing there (faster)
     def unpack_dcms(self, fdir):
-        self.rawscan_path = os.path.normpath(str(fdir))
+        
+        # Getting info about the scan we're working with...
+        self.rawscan_path = os.path.normpath(fdir)
         self.rawscan_dirname = os.path.basename(
             os.path.normpath(self.rawscan_path))
-        os.mkdir(os.path.join(self.tmpdir, self.rawscan_dirname))
-        self.tmpdest = os.path.join(self.tmpdir, self.rawscan_dirname)
+        self.rawscan_type = self.rawscan_dirname.split('_')[1]
+        self.helpful_type = self.scan2helpful(self.rawscan_type)
+        print("\n" + stru(str("FOUND " + self.helpful_type + " SCAN")) + ": " + self.rawscan_path + "\n")
+
+        # Copying the scan's bzip files to /tmp/<scan_type> and decompressing there (time saver)
+        os.mkdir(os.path.join(self.subjID_tmpdir, self.rawscan_dirname))
+        self.tmpdest = os.path.join(self.subjID_tmpdir, self.rawscan_dirname)
         copy_tree(self.rawscan_path, self.tmpdest)
+
+        # Decompressing scan files
+        print(stru("Step 1") + ": Decompressing the raw DICOM archive files...\n")
         bz2_list = (z for z in sorted(os.listdir(
             self.tmpdest)) if z.endswith('.bz2'))
         for filename in bz2_list:
             filepath = os.path.join(self.tmpdest, filename)
-            newfilepath = os.path.join(
+            newfilepath = Path(
                 self.tmpdest, filename.replace(".bz2", ""))
             with open(newfilepath, 'wb') as new_file, open(filepath, 'rb') as file:
                 decompressor = bz2.BZ2Decompressor()
                 for data in iter(lambda: file.read(100 * 1024), b''):
                     new_file.write(decompressor.decompress(data))
-        self.orig_path = self.rawscan_path
+                    sys.stdout.write('.')
+
         self.rawscan_path = self.tmpdest
         self.rawscan_dirname = os.path.basename(
             os.path.normpath(self.rawscan_path))
 
+    # Extracting all the info we'll need to name the file accordings to BIDS and create the JSON sidecar
     def organize_dcms(self):
-        # --Full path to the directory containing the raw dcm files - PASS TO dcm_conv
-        self.rawscan_path = ''
+        print(stru("Step 2") + ": Extracting the relevant BIDS parameters...\n")
 
-        self.rawscan_path = self.tmpdest
-        # --Grabbing the sequence number from the name of the directory holding the raw dcms
-        rawscan_seqno = int(self.rawscan_dirname.split('_')[0][1:])
-        # --Grabbing the type of scan from the name of the directory holding the raw dcms
-        self.rawscan_type = self.rawscan_dirname.split('_')[1]
+        # Each scan folder should contain a YAML file with the scan info
+        yaml_filepath = Path(self.rawscan_path, self.rawscan_dirname + '.yaml')
 
-        self.helpful_type = ''
-        self.helpful_type = self.scan2helpful(self.rawscan_type)
-        yamlfile = os.path.join(
-            self.rawscan_path, self.rawscan_dirname + '.yaml')
+        if not yaml_filepath.exists():
+            print("ERROR: A YAML file was not found in this scan's directory: " + self.rawscan_path)
+            sys.exit(1)
 
-        bids_runno = ''
-        bids_tasklabel = ''
-
-        with open(yamlfile, "r") as yfile:
+        with open(yaml_filepath, "r") as yfile:
             for line in yfile:
                 if line.startswith("  SeriesDescription:"):
                     bids_acqlabel = line.split(': ')[1]
+                    self.bids_sidecar_taskname = bids_acqlabel.replace("\n", "")
                     bids_acqlabel = bids_acqlabel.strip()
                     for c in ['(', ')', '-', ' ']:
                         if c in bids_acqlabel:
@@ -159,21 +165,22 @@ class BidsConv():
                     bids_acqlabel = bids_acqlabel.replace(" ", "")
                     bids_acqlabel = "_acq-" + bids_acqlabel
                     break
+
         if self.rawscan_type.__contains__('epi'):
             bids_tasklabel = bids_acqlabel.replace("_acq-", "")
             bids_tasklabel = "_task-" + bids_tasklabel
             bids_acqlabel = ''
-            with open(yamlfile, "r") as yfile2:
+            with open(yaml_filepath, "r") as yfile2:
                 for line in yfile2:
                     if line.startswith("  SeriesNumber: "):
                         bids_runno = line.split(': ')[1]
                         bids_runno = "_run-" + bids_runno
                         break
+        else:
+            bids_tasklabel = ''
 
         # --Creating common fields
-        # ----bids_scanecho
-        bids_scanecho = '_echo-%e' if self.rawscan_type.__contains__(
-            'fmap') else ''
+
         # ---bids_scansession: the wave of data collection formatted as a BIDS label string
         self.bids_scansession = "_ses-" + str(self.wave_no).zfill(2)
         self.bids_scansessiondir = "ses-" + str(self.wave_no).zfill(2)
@@ -182,7 +189,7 @@ class BidsConv():
             if bids_acqlabel.__contains__('EPI'):
                 self.bids_scanmode = '_epirawfmap'
             elif bids_acqlabel.__contains__('DTI'):
-                self.bids_scanmode = '_dtirawfmap'
+                self.bids_scanmode = '_dwirawfmap'
         else:
             self.bids_scanmode = self.scan2bidsmode(self.rawscan_type)
         # ---bids_participantID: the subject ID formatted as a BIDS label string
@@ -200,10 +207,18 @@ class BidsConv():
             self.bids_scanmode
 
     def conv_dcms(self):
+        print(stru("Step 3") + ": Converting to NIFTI using dcm2niix and sorting into appropriate BIDS folder...\n")
         os.makedirs(self.dcm2niix_outdir, exist_ok=True)
         # print("Running command: dcm2niix" + " -f " + self.dcm2niix_label + " -o " + self.dcm2niix_outdir + " " + self.rawscan_path)
         subprocess.run(["dcm2niix", "-f", self.dcm2niix_label,
                         "-o", self.dcm2niix_outdir, self.rawscan_path])
+        if self.rawscan_type.__contains__('epi'):
+            jsonfilepath = os.path.join(self.dcm2niix_outdir, self.dcm2niix_label + '.json')
+            with open(jsonfilepath) as jsonfile:
+                sidecar = json.load(jsonfile)
+            sidecar['TaskName'] = self.bids_sidecar_taskname
+            with open(jsonfilepath, 'w') as f:
+                json.dump(sidecar, f)
 
     def make_fmap(self, scan_type):
         self.fmap_dir = os.path.join(
@@ -242,36 +257,32 @@ class BidsConv():
             # What scans does the field map go with?
             fmap_intendedfor_path = os.path.join(
                 self.outputpath, self.bids_participantID, self.bids_scansessiondir, 'func')
-            print(fmap_intendedfor_path)
-            fmap_intendedfor = ''
+            fmap_intendedfor_dict = {}
             for scan in os.listdir(fmap_intendedfor_path):
                 if scan.endswith('.nii'):
-                    print(scan)
-                    fmap_intendedfor += '"' + self.bids_scansessiondir + '/func/' + scan + '"' + ','
-            fmap_intendedfor = "[" + fmap_intendedfor[:-1] + "]"
-            print(fmap_intendedfor)
-            self.printu("\n" + "CREATING FIELD MAP FOR EPI SCANS:")
-        elif scan_type.__contains__('dti'):
+                    fmap_intendedfor_dict.setdefault("IntendedFor", []).append(
+                        self.bids_scansessiondir + '/func/' + scan)
+            print("\n" + stru("CREATING FIELD MAP FOR EPI SCANS:"))
+        elif scan_type.__contains__('dwi'):
             wrappedphasefile = rawfmapfile_1.replace(
-                '_dtirawfmap_e1', '_wrapped_phasediff')
+                '_dwirawfmap_e1', '_wrapped_phasediff')
             magoutfile1 = rawfmapfile_1.replace(
-                '_dtirawfmap_e1', '_magnitude1')
+                '_dwirawfmap_e1', '_magnitude1')
             magoutfile2 = rawfmapfile_1.replace(
-                '_dtirawfmap_e1', '_magnitude2')
+                '_dwirawfmap_e1', '_magnitude2')
             phasediffileRads = rawfmapfile_1.replace(
-                '_dtirawfmap_e1', '_phasediff_rads')
+                '_dwirawfmap_e1', '_phasediff_rads')
             phasediffileHz = rawfmapfile_1.replace(
-                '_dtirawfmap_e1', '_phasediff')
+                '_dwirawfmap_e1', '_phasediff')
             # What scans does the field map go with?
             fmap_intendedfor_path = os.path.join(
-                self.outputpath, self.bids_participantID, self.bids_scansessiondir, 'func')
-            print(fmap_intendedfor_path)
-            fmap_intendedfor = ''
+                self.outputpath, self.bids_participantID, self.bids_scansessiondir, 'dwi')
+            fmap_intendedfor_dict = {}
             for scan in os.listdir(fmap_intendedfor_path):
                 if scan.endswith('.nii'):
-                    fmap_intendedfor += '\"' + self.bids_scansessiondir + '/func/' + scan + '\"'
-            fmap_intendedfor = "[" + fmap_intendedfor[:-2] + "]"
-            self.printu("\n" + "CREATING FIELD MAP FOR DTI SCANS:")
+                    fmap_intendedfor_dict.setdefault("IntendedFor", []).append(
+                        self.bids_scansessiondir + '/dwi/' + scan)
+            print("\n" + stru("CREATING FIELD MAP FOR DTI SCANS:"))
 
         calc_computephase = "atan2((b*c-d*a),(a*c+b*d))"
         calc_extractmag = "a"
@@ -279,41 +290,39 @@ class BidsConv():
         calc_rads2hz = "a*1000/" + str(te_diff)
 
         fmap_description = {
-            "EchoTime1": "7", 
-            "EchoTime2": "10"
+            "EchoTime1": ".007",
+            "EchoTime2": ".010"
         }
 
-        fmap_description['IntendedFor:'] = fmap_intendedfor
+        fmap_description.update(fmap_intendedfor_dict)
 
         # -Output the BIDS sidecar file describing this field map
-        json_phasediffile = phasediffileHz.replace(".nii",".json")
+        json_phasediffile = phasediffileHz.replace(".nii", ".json")
         with open(json_phasediffile, 'w') as outfile:
-            json.dump(json.loads(str(fmap_description)), outfile)
+            json.dump(fmap_description, outfile)
 
-        self.printu(
-            "\n" + "  Computing phase difference from raw fieldmap volume:")
+        print("\n" + stru("Step 1") + ": Computing phase difference from raw fieldmap volume")
         # -Computing the phase difference from the raw fieldmap volume
         subprocess.call(["3dcalc", "-float", "-a", rawfmapfile_1v2, "-b", rawfmapfile_1v3, "-c", rawfmapfile_2v2, "-d", rawfmapfile_2v3,
                          "-expr", calc_computephase, "-prefix", wrappedphasefile])
 
-        self.printu("\n" + "  Computing magnitude image #1:")
+        print("\n" + stru("Step 2") + ": Computing magnitude image #1")
         # -Extract magnitude image from first raw fieldmap volume
         subprocess.call(["3dcalc", "-a", rawfmapfile_1v0,
                          "-expr", calc_extractmag, "-prefix", magoutfile1])
 
-        self.printu("\n" + "  Computing magnitude image #2:")
+        print("\n" + stru("Step 3") + ": Computing magnitude image #2")
         # -Extract magnitude image from second raw fieldmap volume
         subprocess.call(["3dcalc", "-a", rawfmapfile_2v0,
                          "-expr", calc_extractmag, "-prefix", magoutfile2])
 
-        self.printu("\n" + "  Unwrapping the phase difference using FSL's 'prelude':")
+        print("\n" + stru("Step 4") + ": Unwrapping the phase difference using FSL's 'prelude'")
         # -Unwrap the phase difference file
         subprocess.call(["prelude", "-v", "-p", wrappedphasefile,
                          "-a", magoutfile1, "-o", phasediffileRads])
         phasediffileRads = phasediffileRads + '.gz'
 
-        self.printu(
-            "\n" + "  Converting the phase difference from radians to Hz:")
+        print("\n" + stru("Step 5") + ": Converting the phase difference from radians to Hz")
         # -Convert the phase difference file from rads to Hz
         # -Formula for conversion: "a" x 1000 / x ; where x is the abs. value of the difference in TE between the two volumes
         subprocess.call(["3dcalc", "-a", phasediffileRads,
@@ -347,17 +356,10 @@ class BidsConv():
                 if not any(x in str(fdir) for x in self.scanstoskip):
                     self.unpack_dcms(fdir)
                     self.organize_dcms()
-                    self.printu("\n" + "FOUND A " + self.helpful_type +
-                                " SCAN: " + self.orig_path + "\n")
-                    self.printu(
-                        "  Step 1: Decompressing the raw DICOM archive file..." + "\n")
-                    self.printu(
-                        "  Step 2: Extracting the relevant BIDS parameters..." + "\n")
-                    self.printu(
-                        "  Step 3: Converting to NIFTI using dcm2niix and sorting into appropriate BIDS folder..." + "\n")
                     self.conv_dcms()
+            #self.addtasknames
             self.make_fmap('epi')
-            self.make_fmap('dti')
+            self.make_fmap('dwi')
             print("\n" + "#"*40 + "\n" + "CONVERSION AND PROCESSING FOR " +
                   self.subjID + " DONE!" + "\n" + "#"*40 + "\n")
         with open(os.path.join(self.outputpath, 'dataset_description.json'), 'w') as outfile:
